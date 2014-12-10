5269b7a17b941e0beb18081380d6e4d2b7176810
Leslie McCutcheon <lesmac88@gmail.com>
2014-12-10 20:45:49 +0000

    Base File (Import)

---
+++ core.js
@@ -0,0 +1,21 @@
+﻿$(function($, undefined) {
+    $('.hover-item').on('mouseenter', function () {
+        $(this).find('div').addClass('display');
+    });
+    $('.hover-item').on('mouseleave', function () {
+        $(this).find('div').removeClass('display');
+    });
+
+    $('.hover-item-animate').on('mouseenter', function () {
+        $(this).find('div').animate(
+        {
+            bottom: "+0%"
+        });
+    });
+    $('.hover-item-animate').on('mouseleave', function () {
+        $(this).find('div').animate(
+         {
+             bottom: "-20%"
+         });
+    });
+}(jQuery));
